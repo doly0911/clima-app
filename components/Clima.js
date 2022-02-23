1fb4d937aece7260b322fabed1b5ff374c43ca5f
doly0911 <dolly.jimenez@udea.edu.co>
2022-02-22 21:51:51 -0500

    Limpieza del proyecto, creacion de componentes, actualizacion gitignore

---
+++ components/Clima.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export default function CLima() {
+  return (
+    <View>
+      <Text>Componente CLima</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
---
+++ components/Formulario.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+export default function Formulario() {
+  return (
+    <View>
+      <Text>Componente formulario</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
