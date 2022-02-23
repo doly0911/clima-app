af48118f4b4a17ca85343a932b294b274c086880
doly0911 <dolly.jimenez@udea.edu.co>
2022-02-22 23:39:46 -0500

    Se agrega picker en el formulario para los paises

--- App.js
+++ App.js
@@ -5,18 +5,13 @@ import Formulario from './Components/Formulario';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Formulario/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
   
 });
--- Components/Formulario.js
+++ Components/Formulario.js
@@ -1,20 +1,34 @@
 import react from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Picker } from "@react-native-picker/picker";
 
 export default function Formulario() {
     return (
-      <View style={styles.container}>
-        <Text>Formularioooo</Text>    
+      <>
+      <View style={styles.formulario}>
+        <View>
+          <TextInput
+          placeholder="Ciudad"
+          placeholderTextColor="#666"/>
+        </View>
+        <View>
+          <Picker>
+            <Picker.Item label="---Seleccione un país---" value=""/>
+            <Picker.Item label="Estados Unidos" value="US"/>
+            <Picker.Item label="México" value="MX"/>
+            <Picker.Item label="Argentina" value="AR"/>
+            <Picker.Item label="Colombia" value="CO"/>
+            <Picker.Item label="Perú" value="PE"/>
+          </Picker>
+        </View>
       </View>
+      </>
     );
   }
   
   const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+    formulario: {
+      marginTop: 100
+    }
   });
   
