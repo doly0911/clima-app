5d3efa3d0734be4b8a7f17d37bdcacf85ee4ea9c
doly0911 <dolly.jimenez@udea.edu.co>
2022-02-23 00:22:52 -0500

    estilos para el formulario

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import Formulario from './Components/Formulario';
 
 export default function App() {
   return (
-    <View >
-      <Formulario/>
+    <View style={styles.app}>
+      <View style={styles.contenido}> 
+         <Formulario/>
+      </View>     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  app: {
+    flex: 1,
+    backgroundColor: '#3E5F8A',
+    justifyContent: 'center'
+  },
+
+  contenido: {
+    marginHorizontal: '5%'
+  }
   
 });
--- Components/Formulario.js
+++ Components/Formulario.js
@@ -1,18 +1,21 @@
 import react from "react";
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableWithoutFeedback } from 'react-native';
 import { Picker } from "@react-native-picker/picker";
 
 export default function Formulario() {
     return (
-      <>
+      
       <View style={styles.formulario}>
+
         <View>
           <TextInput
+          style={styles.input}
           placeholder="Ciudad"
           placeholderTextColor="#666"/>
         </View>
+
         <View>
-          <Picker>
+          <Picker style={styles.picker}>
             <Picker.Item label="---Seleccione un país---" value=""/>
             <Picker.Item label="Estados Unidos" value="US"/>
             <Picker.Item label="México" value="MX"/>
@@ -21,14 +24,48 @@ export default function Formulario() {
             <Picker.Item label="Perú" value="PE"/>
           </Picker>
         </View>
+        
+        <TouchableWithoutFeedback>
+          <View style={styles.btnBuscar}>
+            <Text style={styles.txtBuscar}>
+              Buscar Clima
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
-      </>
+      
     );
   }
   
   const styles = StyleSheet.create({
     formulario: {
       marginTop: 100
+    },
+    input: {
+      padding: 10,
+      height:50,
+      backgroundColor: '#fff',
+      fontSize: 20,
+      marginBottom: 20,
+      textAlign: 'center'
+    },
+    picker: {
+      height: 120, 
+      backgroundColor: '#fff'
+    },
+    btnBuscar:{
+      marginTop: 50,
+      backgroundColor: '#000',
+      padding: 10,
+      justifyContent: 'center'      
+    },
+    txtBuscar:{
+      color: '#fff',
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      fontSize: 18
+
     }
   });
   
